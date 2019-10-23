85e87fbcd29a0377ac1749ae3d1181ac7a195280
Simon Primetzhofer <simon.primetzhofer@live.at>
2019-10-23 15:57:42 +0200

    Added controller for Groups

---
+++ OrderAPI/Controllers/GroupsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrderAPI.Model;
+
+namespace OrderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupsController : ControllerBase
+    {
+        private readonly OrderContext _context;
+
+        public GroupsController(OrderContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Groups
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Group>>> GetGroups()
+        {
+            return await _context.Groups.ToListAsync();
+        }
+    }
+}
